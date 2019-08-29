56f7aaa83f05c01d22fe243b6f473b6337c0f1a2
Jorge Betancur <jabetancur12@gmail.com>
2019-08-29 01:23:33 -0500

    Updates

---
+++ precache-manifest.e8f99e921dbcf7df8e81bc544103b525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4faecfd01b1faa0f7cb7d3ea262865",
+    "url": "/postMasVotados/index.html"
+  },
+  {
+    "revision": "f68547f6dd8500c9018a",
+    "url": "/postMasVotados/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a0f927a1bb09d29ad0a2",
+    "url": "/postMasVotados/static/css/main.4d0045a0.chunk.css"
+  },
+  {
+    "revision": "f68547f6dd8500c9018a",
+    "url": "/postMasVotados/static/js/2.03156084.chunk.js"
+  },
+  {
+    "revision": "a0f927a1bb09d29ad0a2",
+    "url": "/postMasVotados/static/js/main.334030a8.chunk.js"
+  },
+  {
+    "revision": "f514dc33fbed3579fab0",
+    "url": "/postMasVotados/static/js/runtime~main.298cf7d2.js"
+  }
+]);
